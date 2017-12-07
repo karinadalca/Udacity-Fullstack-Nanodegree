baa03ebf4c9e79ccdf6f9910ef2d9d96a1f42b2a
Karina Dalca <karinadalca@gmail.com>
2017-12-07 12:26:28 -0800

    movies class

---
+++ Movies/entertainmentCenter.py
@@ -0,0 +1,18 @@
+import media
+
+toy_story = media.Movie("Toy Story", 
+"A story of a boy and his toys that come to life", 
+"http://www.gstatic.com/tv/thumb/movieposters/17420/p17420_p_v8_ab.jpg", 
+"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar", 
+"A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.", 
+"https://images-na.ssl-images-amazon.com/images/I/91qDRmPbAjS._SL1500_.jpg", 
+"https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+coco = media.Movie("Coco", 
+"Aspiring musician Miguel, confronted with his family's ancestral ban on music, enters the Land of the Dead to find his great-great-grandfather, a legendary singer.", 
+"http://t3.gstatic.com/images?q=tbn:ANd9GcS3FGIsangc2iauB89mttkiBAvIDj_O952Ypk2p5QqABby--L6d", 
+"https://www.youtube.com/watch?v=Ga6RYejo6Hk")
+
+coco.show_trailer()
